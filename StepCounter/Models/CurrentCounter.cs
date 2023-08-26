0bc6140ba6ee8673ec4d4588ce205bdfad838e02
rachnanaithani <naithani.chetan@gmail.com>
2023-08-27 02:30:09 +0530

    Adding SQLite

---
+++ StepCounter/Models/CurrentCounter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StepCounter.Models
+{
+	public class CurrentCounter
+	{
+        [SQLite.PrimaryKey]
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Steps { get; set; }
+    }
+}
+
---
+++ StepCounter/Models/Step.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StepCounter.Models
+{
+	public class Step
+	{
+        [SQLite.PrimaryKey]
+        public int Id { get; set; }
+        public Boolean IsActive { get; set; }
+        public int ServerId { get; set; }
+        public DateTime Modified { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Steps { get; set; }
+    }
+}
+
