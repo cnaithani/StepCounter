56ccab6279df8eb6e0710a3169db0f96a101a2f7
rachnanaithani <naithani.chetan@gmail.com>
2023-09-17 03:23:54 +0530

    Android Add main donut chart

--- StepCounter/Charts/Pie/PieChartDrawable.cs
+++ StepCounter/Charts/Pie/PieChartDrawable.cs
@@ -5,6 +5,8 @@ namespace StepCounter.Charts.Pie
 {
 	internal class PieChartDrawable : View, IDrawable
     {
+        int xOffset = 300;
+        int yOffset = 200;
         public static readonly BindableProperty PointsProperty = BindableProperty.Create(nameof(Points),
             typeof(Dictionary<string, float>),
             typeof(PieChartDrawable),
@@ -31,31 +33,51 @@ namespace StepCounter.Charts.Pie
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
             canvas.ResetState();
-            var radius = dirtyRect.Width / 4;
+            var radius = dirtyRect.Width ;
             var purple = Color.FromRgba(178, 127, 255, 125);
             var translucent = Color.FromRgba(235, 222, 255, 0);
+            var white = Color.FromRgba(0, 0, 0, 255);
             canvas.FontColor = Color.FromArgb("#7F2CF6");
-            var center = new PointF(dirtyRect.Center.X, dirtyRect.Center.Y);
-            //Draw Circle 
+            var center = new PointF(xOffset + dirtyRect.Center.X, yOffset + dirtyRect.Center.Y);
+            //Draw Outer Circle 
             var radialGradientPaint = new RadialGradientPaint
             {
                 EndColor = purple,
-                StartColor = translucent
+                StartColor = purple
             };
 
             var radialRectangle = new RectF(dirtyRect.Center.X - radius, dirtyRect.Center.Y - radius, radius * 2, radius * 2);
             canvas.SetFillPaint(radialGradientPaint, radialRectangle);
             canvas.FillCircle(center, radius);
+
+
+            //Draw Inner Circle 
+            var radialGradientPaintInner = new RadialGradientPaint
+            {
+                EndColor = white,
+                StartColor = white
+            };
+
+            var radialRectangleInner = new RectF(dirtyRect.Center.X - radius + 100, dirtyRect.Center.Y - radius + 100, radius * 2 - 200, radius * 2 - 200);
+            canvas.SetFillPaint(radialGradientPaintInner, radialRectangleInner);
+            canvas.FillCircle(center, radius-100);
+
+            /*
             var scale = 100f / Points.Select(x => x.Value).Sum();
+            */
+            /*
             //Draw first initial line
             canvas.StrokeColor = Colors.White;
             canvas.DrawLine(
                 new PointF(center.X, center.Y - radius),
                 center);
+            */
+            /*
             var lineDegrees = 0f;
             var textDegrees = 0f;
             var textRadiusPadding = Convert.ToInt32(dirtyRect.Width / 10);
             //Draw splits into pie using 𝝅
+            
             for (var i = 0; i < Points.Count; i++)
             {
                 var point = Points.ElementAt(i);
@@ -77,6 +99,7 @@ namespace StepCounter.Charts.Pie
                    HorizontalAlignment.Center);
                 textDegrees += (360 * (point.Value * scale / 100) / 2);
             }
+            */
         }
     }
 }
